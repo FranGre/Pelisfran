1c7979be142db315a2c5ff20b21eeceee35a2f07
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-18 20:51:36 +0100

    [FEAT] - Creado Modelo Rol junto con sus propiedades

---
+++ Pelisfran/Modelos/Rol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pelisfran.Modelos
+{
+    [Table("Roles")]
+    public class Rol
+    {
+        public Guid Id { get; set; }
+        [MaxLength(30)]
+        public string Tipo { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime? ActualizadoEn { get; set; }
+    }
+}
