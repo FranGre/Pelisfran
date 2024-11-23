8e5c67e6bc7b1f06eaf3e7d15ae5d39852fbb155
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-23 22:43:44 +0100

    [ADD] - GeneroPeliculaServicio y metodo AgregarGeneroAPelicula

---
+++ Pelisfran/Servicios/GeneroPeliculaServicio.cs
@@ -0,0 +1,20 @@
+﻿using Pelisfran.Modelos;
+using Pelisfran.Repositorios;
+
+namespace Pelisfran.Servicios
+{
+    public class GeneroPeliculaServicio
+    {
+        private GeneroPeliculaRepositorio _generoPeliculaRepositorio;
+
+        public GeneroPeliculaServicio()
+        {
+            _generoPeliculaRepositorio = new GeneroPeliculaRepositorio();
+        }
+
+        public void AgregarGeneroAPelicula(GeneroPelicula generoPelicula)
+        {
+            _generoPeliculaRepositorio.Crear(generoPelicula);
+        }
+    }
+}
