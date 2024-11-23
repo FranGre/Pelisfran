0afc4a161aed2ab9135146c51de568ecf5634f6f
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-23 22:38:15 +0100

    [ADD] - GeneroPeliculaRepositorio con metodo Crear(()

---
+++ Pelisfran/Repositorios/GeneroPeliculaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class GeneroPeliculaRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public GeneroPeliculaRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(GeneroPelicula generoPelicula)
+        {
+            _db.GenerosPeliculas.Add(generoPelicula);
+            _db.SaveChanges();
+        }
+    }
+}
