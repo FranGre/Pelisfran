60a4aeabb4ab472b5af0f1b44a4daf9b12847f0c
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-23 22:30:17 +0100

    [ADD] - PeliculaRepositorio

---
+++ Pelisfran/Repositorios/PeliculaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class PeliculaRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public PeliculaRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(Pelicula pelicula)
+        {
+            _db.Peliculas.Add(pelicula);
+            _db.SaveChanges();
+        }
+    }
+}
