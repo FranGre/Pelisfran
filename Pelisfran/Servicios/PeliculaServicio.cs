58ae5f9515ea4aa1619f1d28cfdf226803f0c0cc
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-23 22:32:50 +0100

    [ADD] - PeliculaServicio con CrearPelicula

---
+++ Pelisfran/Servicios/PeliculaServicio.cs
@@ -0,0 +1,20 @@
+﻿using Pelisfran.Modelos;
+using Pelisfran.Repositorios;
+
+namespace Pelisfran.Servicios
+{
+    public class PeliculaServicio
+    {
+        private PeliculaRepositorio _peliculaRepositorio;
+
+        public PeliculaServicio()
+        {
+            _peliculaRepositorio = new PeliculaRepositorio();
+        }
+
+        public void CrearPelicula(Pelicula pelicula)
+        {
+            _peliculaRepositorio.Crear(pelicula);
+        }
+    }
+}
