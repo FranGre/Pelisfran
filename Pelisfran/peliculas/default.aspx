c361e7faabe4b0646411b1bcc9f43e71d27bc632
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-25 23:45:33 +0100

    [FEAT] - Creada vista para mostrar todas las peliculas, ademas las muestra de forma basica

---
+++ Pelisfran/peliculas/default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Pelisfran.peliculas._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Peliculas</h2>
+            <asp:Repeater ID="repPeliculas" runat="server" OnItemDataBound="repPeliculas_ItemDataBound">
+                <ItemTemplate>
+                    <div>
+                        <asp:Literal id="titulo" runat="server"></asp:Literal>
+                        <asp:Literal id="fechaLanzamiento" runat="server"></asp:Literal>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pelisfran/peliculas/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace Pelisfran.peliculas
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        private PelisFranDBContexto _db = new PelisFranDBContexto();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                var peliculas = _db.Peliculas.ToList();
+                repPeliculas.DataSource = peliculas;
+                repPeliculas.DataBind();
+            }
+        }
+
+        protected void repPeliculas_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            RepeaterItem repeaterItem = e.Item;
+
+            Literal titulo = (Literal)repeaterItem.FindControl("titulo");
+            Literal fechaLanzamiento = (Literal)repeaterItem.FindControl("fechaLanzamiento");
+
+            Pelicula pelicula = (Pelicula)repeaterItem.DataItem;
+
+            titulo.Text = pelicula.Titulo;
+            fechaLanzamiento.Text = pelicula.FechaLanzamiento.ToShortDateString();
+        }
+    }
+}
