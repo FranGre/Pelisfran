d98c8fac0839e79d569a2ce27767f28cca61403f
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-26 20:13:55 +0100

    [ADD] - Credo PeliculaFavoritaRepositorio con metodo Crear

---
+++ Pelisfran/Repositorios/PeliculaFavoritaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class PeliculaFavoritaRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public PeliculaFavoritaRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(PeliculaFavorita peliculaFavorita)
+        {
+            _db.PeliculasFavoritas.Add(peliculaFavorita);
+            _db.SaveChanges();
+        }
+    }
+}
