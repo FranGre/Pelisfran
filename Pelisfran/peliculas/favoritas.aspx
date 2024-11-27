b03a096dc97a6db383f56854f1f808ec84d3aef3
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-27 19:49:34 +0100

    [FEAT] - creada vista favoritas, me muestra mis peliculas marcadas como favoritas

---
+++ Pelisfran/peliculas/favoritas.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaestras/Site1.Master" AutoEventWireup="true" CodeBehind="favoritas.aspx.cs" Inherits="Pelisfran.peliculas.favoritas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <asp:Repeater ID="repPeliculas" runat="server" OnItemDataBound="repPeliculas_ItemDataBound">
+        <ItemTemplate>
+            <div>
+                <asp:Image ID="portada" runat="server" />
+                <h4 id="titulo" runat="server"></h4>
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
+</asp:Content>
---
+++ Pelisfran/peliculas/favoritas.aspx.cs
@@ -0,0 +1,37 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Pelisfran.peliculas
+{
+    public partial class favoritas : Page
+    {
+        private PelisFranDBContexto _db = new PelisFranDBContexto();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Guid usuarioId = Guid.Parse(HttpContext.Current.User.Identity.Name);
+            var misPeliculasFavoritas = _db.PeliculasFavoritas.Where(peliculaFavorita => peliculaFavorita.UsuarioId == usuarioId).ToList();
+            repPeliculas.DataSource = misPeliculasFavoritas;
+            repPeliculas.DataBind();
+        }
+
+        protected void repPeliculas_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            Image portada = (Image)e.Item.FindControl("portada");
+            HtmlGenericControl titulo = (HtmlGenericControl)e.Item.FindControl("titulo");
+
+            PeliculaFavorita miPeliculaFavorita = (PeliculaFavorita)e.Item.DataItem;
+            Pelicula pelicula = _db.Peliculas.Include("PortadaPelicula").Where(item => item.Id == miPeliculaFavorita.PeliculaId).FirstOrDefault();
+
+            var imageUrl = ResolveUrl($"{pelicula.PortadaPelicula.Ruta}/{pelicula.PortadaPelicula.NombreOriginal}");
+            portada.ImageUrl = imageUrl;
+            titulo.InnerText = pelicula.Titulo;
+        }
+    }
+}
