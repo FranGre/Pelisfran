8321faa6a28c279d550bbcc09a5a11908160ade4
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-28 20:30:13 +0100

    [ADD] - agregada la navegacion peliculas en el modelo Usuario

--- Pelisfran/Modelos/Usuario.cs
+++ Pelisfran/Modelos/Usuario.cs
@@ -27,6 +27,7 @@ namespace Pelisfran.Modelos
         public Rol Rol { get; set; }
         public ICollection<Genero> Generos { get; set; }
         public ICollection<PeliculaFavorita> PeliculaFavoritas { get; set; }
+        public ICollection<Pelicula> Peliculas { get; set; }
         public ICollection<Serie> Series { get; set; }
     }
 }
