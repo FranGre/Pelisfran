be324b97215e151eef240810aa6f427216c02edd
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-28 20:20:14 +0100

    [ADD] - Modelo Serie

---
+++ Pelisfran/Modelos/Serie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pelisfran.Modelos
+{
+    public class Serie
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string SinopsisBreve { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime? ActualizadoEn { get; set; }
+    }
+}
