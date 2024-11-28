3ebd9e8cfe3cbe58eb09888f9a54444afad997a7
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-28 20:17:28 +0100

    [ADD] - carpeta series con su vista crear a medio hacer

---
+++ Pelisfran/series/crear.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaestras/Site1.Master" AutoEventWireup="true" CodeBehind="crear.aspx.cs" Inherits="Pelisfran.series.crear" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <h2>Nueva Serie</h2>
+
+    <div>
+        <asp:Label ID="lbTitulo" runat="server" Text="Titulo" />
+        <asp:TextBox ID="txtTitulo" runat="server" />
+        <asp:RequiredFieldValidator ID="reqTitulo" runat="server" ControlToValidate="txtTitulo" ErrorMessage="Campo obligatorio" Display="Dynamic" />
+    </div>
+
+    <div>
+        <asp:Label ID="lbSinopsisBreve" runat="server" Text="Sinopsis" />
+        <asp:TextBox ID="txtSinopsis" runat="server" />
+        <asp:RequiredFieldValidator ID="reqSinopsis" runat="server" ControlToValidate="txtSinopsis" ErrorMessage="Campo obligatorio" Display="Dynamic" />
+    </div>
+
+    <div>
+        <asp:Label ID="lbFechaLanzamiento" runat="server" Text="Fecha Lanzamiento" />
+        <asp:TextBox ID="txtFechaLanzamiento" runat="server" TextMode="Date" />
+        <asp:RequiredFieldValidator ID="reqFechaLanzamiento" runat="server" ControlToValidate="txtFechaLanzamiento" ErrorMessage="Campo obligatorio" Display="Dynamic" />
+    </div>
+
+    <asp:Button ID="btnAceptar" runat="server" Text="Crear" OnClick="btnAceptar_Click" />
+</asp:Content>
---
+++ Pelisfran/series/crear.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Pelisfran.series
+{
+
+
+    public partial class crear
+    {
+
+        /// <summary>
+        /// Control lbTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbTitulo;
+
+        /// <summary>
+        /// Control txtTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTitulo;
+
+        /// <summary>
+        /// Control reqTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqTitulo;
+
+        /// <summary>
+        /// Control lbSinopsisBreve.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbSinopsisBreve;
+
+        /// <summary>
+        /// Control txtSinopsis.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSinopsis;
+
+        /// <summary>
+        /// Control reqSinopsis.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqSinopsis;
+
+        /// <summary>
+        /// Control lbFechaLanzamiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbFechaLanzamiento;
+
+        /// <summary>
+        /// Control txtFechaLanzamiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaLanzamiento;
+
+        /// <summary>
+        /// Control reqFechaLanzamiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqFechaLanzamiento;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
+    }
+}
