611b9c0117475a36934e84c4b9fec06af76a7354
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-29 18:47:09 +0100

    [ADD] - Creado modelo Temporada junto con las claves ajenas UsuarioId y SerieId, incluidas sus respectivas navegaciones

---
+++ Pelisfran/Modelos/Temporada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pelisfran.Modelos
+{
+    public class Temporada
+    {
+        public Guid Id { get; set; }
+        public byte NumeroTemporada { get; set; }
+        public string SinopsisBreve { get; set; }
+        public DateTime? FechaLanzamiento { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime? ActualizadoEn { get; set; }
+
+        [ForeignKey("Usuario")]
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        [ForeignKey("Serie")]
+        public Serie SerieId { get; set; }
+        public Serie Serie { get; set; }
+    }
+}
