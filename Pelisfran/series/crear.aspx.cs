9ab5a44712bb479129fb10bf084dd958e4af967e
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-29 17:58:56 +0100

    [FEAT] - permite crear serie

--- Pelisfran/series/crear.aspx.cs
+++ Pelisfran/series/crear.aspx.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+using System;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Pelisfran.series
 {
-    public partial class crear : System.Web.UI.Page
+    public partial class crear : Page
     {
+        PelisFranDBContexto _db = new PelisFranDBContexto();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -18,7 +19,19 @@ namespace Pelisfran.series
         {
             if (!Page.IsValid) { return; }
 
+            Serie serie = new Serie
+            {
+                Id = Guid.NewGuid(),
+                Titulo = txtTitulo.Text,
+                SinopsisBreve = txtSinopsis.Text,
+                FechaLanzamiento = Convert.ToDateTime(txtFechaLanzamiento.Text),
+                CreadoEn = DateTime.Now,
+                UsuarioId = Guid.Parse(HttpContext.Current.User.Identity.Name)
+            };
 
+
+            _db.Series.Add(serie);
+            _db.SaveChanges();
         }
     }
 }
