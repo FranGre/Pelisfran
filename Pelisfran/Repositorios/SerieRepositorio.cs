e492146d91b06cb88d5cc363b05fa4d30c30b02c
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-29 20:20:52 +0100

    [ADD] - SerieRepositorio con metodo Crear

---
+++ Pelisfran/Repositorios/SerieRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class SerieRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public SerieRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(Serie serie)
+        {
+            _db.Series.Add(serie);
+            _db.SaveChanges();
+        }
+    }
+}
