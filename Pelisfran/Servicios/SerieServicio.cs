3e4d47cc047597840a9cfa48e839d1feeea46191
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-29 20:22:59 +0100

    [ADD] - SerieServicio con metodo CrearSerie

---
+++ Pelisfran/Servicios/SerieServicio.cs
@@ -0,0 +1,20 @@
+﻿using Pelisfran.Modelos;
+using Pelisfran.Repositorios;
+
+namespace Pelisfran.Servicios
+{
+    public class SerieServicio
+    {
+        private SerieRepositorio _serieRepositorio;
+
+        public SerieServicio()
+        {
+            _serieRepositorio = new SerieRepositorio();
+        }
+
+        public void CrearSerie(Serie serie)
+        {
+            _serieRepositorio.Crear(serie);
+        }
+    }
+}
