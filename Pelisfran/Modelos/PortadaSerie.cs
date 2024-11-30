79acf5af5927f8fb6b3a7ac821b7debb031df1b6
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-30 19:45:21 +0100

    [ADD] - modelo PortadaSerie con la validacion del modelo

---
+++ Pelisfran/Modelos/PortadaSerie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Pelisfran.Modelos
+{
+    public class PortadaSerie
+    {
+        [ForeignKey("Serie")]
+        public Guid Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string NombreOriginal { get; set; }
+        [Required]
+        public string Extension { get; set; }
+        [Required]
+        public string Ruta { get; set; }
+        public Serie Serie { get; set; }
+    }
+}
