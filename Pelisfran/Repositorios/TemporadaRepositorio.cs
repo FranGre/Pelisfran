ea5318da4cbe820b1549889e3a2de758c2630837
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-30 19:10:49 +0100

    [FEAT] - Creado TemporadaRepositorio y metodo Crear

---
+++ Pelisfran/Repositorios/TemporadaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class TemporadaRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public TemporadaRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(Temporada temporada)
+        {
+            _db.Temporadas.Add(temporada);
+            _db.SaveChanges();
+        }
+    }
+}
