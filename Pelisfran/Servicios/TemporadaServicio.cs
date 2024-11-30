38315d168e87851474f3ec528592b60c8954b502
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-11-30 19:16:31 +0100

    [FEAT] - Creado TemporadaServicio y su metodo CrearTemporada

---
+++ Pelisfran/Servicios/TemporadaServicio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Modelos;
+using Pelisfran.Repositorios;
+using System;
+
+namespace Pelisfran.Servicios
+{
+    public class TemporadaServicio
+    {
+        private TemporadaRepositorio _temporadaRepositorio;
+
+        public TemporadaServicio()
+        {
+            _temporadaRepositorio = new TemporadaRepositorio();
+        }
+
+        public void CrearTemporada(Temporada temporada)
+        {
+            _temporadaRepositorio.Crear(temporada);
+        }
+    }
+}
