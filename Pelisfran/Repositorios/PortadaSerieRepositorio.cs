bdbc6ed2ac348f43b35bc26728740e5927c3c757
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-12-03 17:13:11 +0100

    [ADD] - PortadaSerieRepositorio y PortadaSerieServicio con su metodo AgregarPortadaASerie

---
+++ Pelisfran/Repositorios/PortadaSerieRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+
+namespace Pelisfran.Repositorios
+{
+    public class PortadaSerieRepositorio
+    {
+        private PelisFranDBContexto _db;
+
+        public PortadaSerieRepositorio()
+        {
+            _db = new PelisFranDBContexto();
+        }
+
+        public void Crear(PortadaSerie portadaSerie)
+        {
+            _db.PortadasSeries.Add(portadaSerie);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Pelisfran/Servicios/PortadaSerieServicio.cs
@@ -0,0 +1,20 @@
+﻿using Pelisfran.Modelos;
+using Pelisfran.Repositorios;
+
+namespace Pelisfran.Servicios
+{
+    public class PortadaSerieServicio
+    {
+        private PortadaSerieRepositorio _portadaSerieRepositorio;
+
+        public PortadaSerieServicio()
+        {
+            _portadaSerieRepositorio = new PortadaSerieRepositorio();
+        }
+
+        public void AgregarPortadaASerie(PortadaSerie portadaSerie)
+        {
+            _portadaSerieRepositorio.Crear(portadaSerie);
+        }
+    }
+}
