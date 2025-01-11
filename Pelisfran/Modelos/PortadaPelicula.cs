9359cb7ecb1509e32008dbaf6c8f4510c78dfee1
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-11 14:16:02 +0100

    [REF]

--- Pelisfran/Modelos/PortadaPelicula.cs
+++ Pelisfran/Modelos/PortadaPelicula.cs
@@ -7,16 +7,16 @@ namespace Pelisfran.Modelos
     [Table("PortadasPeliculas")]
     public class PortadaPelicula
     {
-    [ForeignKey("Pelicula")]
-    public Guid Id { get; set; }
-    [Required]
-    public string Nombre { get; set; }
-    [Required]
-    public string NombreOriginal { get; set; }
-    [Required]
-    public string Extension { get; set; }
-    [Required]
-    public string Ruta { get; set; }
-    public Pelicula Pelicula { get; set; }
+        [ForeignKey("Pelicula")]
+        public Guid Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string NombreOriginal { get; set; }
+        [Required]
+        public string Extension { get; set; }
+        [Required]
+        public string Ruta { get; set; }
+        public Pelicula Pelicula { get; set; }
     }
 }
