33d42b678bf4cf3f1648f11b8ac023bf03e2a6c4
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-14 22:02:52 +0100

    [ADD] - Base.cs

---
+++ Pelisfran/Core/Base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Pelisfran.Core
+{
+    public class Base : Page
+    {
+        protected Guid usuarioId
+        {
+            get { return Guid.Parse(HttpContext.Current.Session["usuarioId"].ToString()); }
+            set { HttpContext.Current.Session["usuarioId"] = value; }
+        }
+
+        protected bool EstaUsuarioAutenticado()
+        {
+            return HttpContext.Current.User.Identity.IsAuthenticated;
+        }
+    }
+}
