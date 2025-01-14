7ae1014fcdd2dffc27433f4c54c65c7ea8d87a35
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-14 23:35:50 +0100

    [ADD] RolServicio.cs

---
+++ Pelisfran/Servicios/RolServicio.cs
@@ -0,0 +1,24 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Modelos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pelisfran.Servicios
+{
+    public class RolServicio
+    {
+        private PelisFranDBContexto _db = new PelisFranDBContexto();
+
+        public List<Rol> roles
+        {
+            get { return (List<Rol>)HttpContext.Current.Session["roles"]; }
+            set { HttpContext.Current.Session["roles"] = value; }
+        }
+
+        public void CargarRoles()
+        {
+            roles = _db.Roles.ToList();
+        }
+    }
+}
