bb42ed5c9a2b7a68e38eceffee484da90dddf0c9
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-14 22:02:14 +0100

    [FEAT] - Al acceder a admin/default.aspx se deniega el acceso si no eres admin

--- Pelisfran/admin/default.aspx.cs
+++ Pelisfran/admin/default.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using Pelisfran.Contexto;
+using Pelisfran.Enums;
 using System;
 using System.Linq;
+using System.Web;
 using System.Web.UI;
 
 namespace Pelisfran.admin
@@ -11,6 +13,15 @@ namespace Pelisfran.admin
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            Guid usuarioId = Guid.Parse(HttpContext.Current.User.Identity.Name);
+
+            var usuario = _db.Usuarios.Find(usuarioId);
+
+            if ((TipoRolesEnum)usuario.RolId != TipoRolesEnum.Administrador)
+            {
+                // acceso denegado
+                return;
+            }
             pGenerosTotal.InnerText = _db.Generos.Count().ToString();
             pPeliculasTotal.InnerText = _db.Peliculas.Count().ToString();
             pUsuariosTotal.InnerText = _db.Usuarios.Count().ToString();
