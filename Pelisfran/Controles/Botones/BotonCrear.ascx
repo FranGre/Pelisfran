e68533eeb7bb8170faad067bd12922e55c733004
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-17 20:15:46 +0100

    [ADD] - Creado Control BotonCrear

---
+++ Pelisfran/Controles/Botones/BotonCrear.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="BotonCrear.ascx.cs" Inherits="Pelisfran.Controles.Botones.BotonCrear" %>
+
+<button id="buttonCrear" runat="server" onserverclick="buttonCrear_ServerClick" class="button is-success is-flex is-align-items-center">
+    <i class="fa-solid fa-plus"></i>
+    <p id="text" runat="server" class="pl-1"></p>
+</button>
---
+++ Pelisfran/Controles/Botones/BotonCrear.ascx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Pelisfran.Controles.Botones
+{
+    public partial class BotonCrear : UserControl
+    {
+        public event EventHandler Click;
+        public string Text { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Text))
+            {
+                return;
+            }
+
+            text.InnerText = this.Text;
+        }
+
+        protected void buttonCrear_ServerClick(object sender, EventArgs e)
+        {
+            Click?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
