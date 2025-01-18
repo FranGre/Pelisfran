c5b2e989d010714ccf93a9d2974bfa3aadfb19ac
Fran Gregori Tandazo <frangregori61@gmail.com>
2025-01-18 11:37:09 +0100

    [FEAT] - admin/usuarios/default al clickar en los comentarios te lleva a ver los comentarios que ha realizado el user

---
+++ Pelisfran/admin/comentarios/usuario.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaestras/Base.Master" AutoEventWireup="true" CodeBehind="usuario.aspx.cs" Inherits="Pelisfran.admin.comentarios.usuario" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <div class="mb-6">
+        <h2 id="titulo" runat="server" class="title is-2 mb-2 is-flex is-justify-content-center">Comentarios</h2>
+        <h3 id="subtitulo" runat="server" class="subtitle is-4 is-flex is-justify-content-center has-text-weight-light is-italic"></h3>
+    </div>
+    <asp:UpdatePanel ID="UpComentarios" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <div class="mx-6">
+                <asp:ListView ID="lvComentarios" runat="server" OnPagePropertiesChanging="lvComentarios_PagePropertiesChanging" OnItemDataBound="lvComentarios_ItemDataBound">
+
+                    <EmptyDataTemplate>No hay comentarios</EmptyDataTemplate>
+
+                    <ItemTemplate>
+                        <div class="mb-4 box">
+                            <div class="flex is-display-flex is-justify-content-space-between">
+                                <div class="is-display-flex">
+                                    <asp:Image ID="fotoPerfil" runat="server" CssClass="image is-rounded is-16x16" />
+                                    <small id="nombre" runat="server" class="ml-1 has-text-weight-semibold"></small>
+                                </div>
+                                <p id="tituloPelicula" runat="server"></p>
+                                <small id="fecha" runat="server" class="ml-6 has-text-weight-semibold"></small>
+                            </div>
+                            <p id="comentario" runat="server" class="ml-3"></p>
+                        </div>
+                    </ItemTemplate>
+
+                    <LayoutTemplate>
+                        <asp:PlaceHolder ID="itemPlaceHolder" runat="server"></asp:PlaceHolder>
+                        <asp:DataPager ID="dpComentarios" runat="server" PagedControlID="lvComentarios" PageSize="10">
+                            <Fields>
+                                <asp:NumericPagerField ButtonType="Button" />
+                            </Fields>
+                        </asp:DataPager>
+                    </LayoutTemplate>
+
+                </asp:ListView>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ Pelisfran/admin/comentarios/usuario.aspx.cs
@@ -0,0 +1,84 @@
+﻿using Pelisfran.Contexto;
+using Pelisfran.Core;
+using Pelisfran.Helpers;
+using Pelisfran.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Pelisfran.admin.comentarios
+{
+    public partial class usuario : Base
+    {
+        private PelisFranDBContexto _db = new PelisFranDBContexto();
+        private Guid idUsuarioQuery = Guid.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] == null)
+            {
+                Response.Redirect("~/autenticado.aspx");
+                return;
+            }
+
+            idUsuarioQuery = Guid.Parse(Request.QueryString["id"]);
+            var usuario = _db.Usuarios.Find(idUsuarioQuery);
+
+            if (usuario == null)
+            {
+                Response.Redirect("~/autenticado.aspx");
+                return;
+            }
+
+            if (!Page.IsPostBack)
+            {
+                subtitulo.InnerText = usuario.NombreUsuario;
+                var comentarios = _db.ComentariosPeliculas.Where(c => c.UsuarioId == this.idUsuarioQuery).ToList();
+                BindearComentarios(comentarios);
+            }
+        }
+
+        private List<ComentarioPelicula> ObtenerComentarios()
+        {
+            return _db.ComentariosPeliculas.Where(c => c.UsuarioId == this.idUsuarioQuery).OrderByDescending(c => c.FechaCreacion).ToList();
+        }
+
+        private void BindearComentarios(List<ComentarioPelicula> comentarios)
+        {
+            lvComentarios.DataSource = comentarios;
+            lvComentarios.DataBind();
+        }
+
+        protected void lvComentarios_PagePropertiesChanging(object sender, PagePropertiesChangingEventArgs e)
+        {
+            DataPager dpComentarios = (DataPager)lvComentarios.FindControl("dpComentarios");
+            dpComentarios.SetPageProperties(e.StartRowIndex, e.MaximumRows, false);
+
+            var comentarios = ObtenerComentarios();
+            BindearComentarios(comentarios);
+        }
+
+        protected void lvComentarios_ItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+            ListViewItem repeaterItem = e.Item;
+
+            HtmlGenericControl nombre = (HtmlGenericControl)repeaterItem.FindControl("nombre");
+            HtmlGenericControl tituloPelicula = (HtmlGenericControl)repeaterItem.FindControl("tituloPelicula");
+            HtmlGenericControl fecha = (HtmlGenericControl)repeaterItem.FindControl("fecha");
+            HtmlGenericControl comentario = (HtmlGenericControl)repeaterItem.FindControl("comentario");
+            Image fotoPerfil = (Image)repeaterItem.FindControl("fotoPerfil");
+
+            ComentarioPelicula comentarioPelicula = (ComentarioPelicula)repeaterItem.DataItem;
+
+            nombre.InnerText = comentarioPelicula.Usuario.NombreUsuario;
+            tituloPelicula.InnerText = comentarioPelicula.Pelicula.Titulo;
+            fecha.InnerText = HelperFecha.ObtenerTiempoTranscurrido(comentarioPelicula.FechaCreacion);
+            comentario.InnerText = comentarioPelicula.Comentario;
+            var usuario = _db.Usuarios.Include("FotoPerfil").Where(u => u.Id == this.idUsuarioQuery).FirstOrDefault();
+            string rutaFotoPerfil = usuario.FotoPerfil.Ruta;
+            fotoPerfil.ImageUrl = $"~/{rutaFotoPerfil}";
+        }
+    }
+}
